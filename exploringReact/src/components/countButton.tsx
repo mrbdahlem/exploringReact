e6e5351d6ecc2ced396891b787430634c3643698
Brian Dahlem <dahlem.brian@cusd80.com>
2024-07-24 17:31:00 +0000

    playing with the button

--- exploringReact/src/components/countButton.tsx
+++ exploringReact/src/components/countButton.tsx
@@ -3,14 +3,14 @@ import { useState } from "react"
 
 export function CountButton() {
     
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
     
-        return (
-            <button onClick={() => setCount((count) => count + 1)} 
-                    className=" text-3xl bg-blue-400 hover:bg-blue-500 p-2 rounded-md my-auto ">
-                count is {count}
-            </button>
-        )
+    return (
+        <button onClick={() => setCount((count) => count + 1)} 
+                className=" text-3xl bg-blue-600 hover:bg-blue-400 p-4 rounded-md my-auto ">
+            count is {count}
+        </button>
+    );
 }
 
 export default CountButton
